42bda32b3e5c1b4c847a127065ac485dd91024df
tuboshums <54980978+tuboshums@users.noreply.github.com>
2019-09-18 00:15:40 +0800

    Update 031702432.py

--- 031702432.py
+++ 031702432.py
@@ -1,40 +1,52 @@
 import json
 import re
 
-def get_address(w):##获取地址
-    addresss=re.sub(r'\d{11}',"",w)
-    addresss=re.findall("\D*.*?,(.*?)\.",addresss) 
-    addresss=("".join(addresss))
-    return(fall(addresss))
+def get_province():
+    pros=[]
+    pros.append(["北京","天津","上海","重庆","河北","山西","辽宁","吉林","黑龙江","江苏","浙江","安徽","福建","江西","山东","河南","湖北","湖南","广东","海南","四川","贵州","云南","陕西","甘肃","青海","台湾","内蒙古自治区","广西壮族自治区","西藏自治区","宁夏回族自治区","新疆维吾尔自治区","香港","澳门"])
+    return pros
 
-def get_phone(w):##获取电话号码
-    number=re.findall("\d{11}",w)
-    return number
-
-def get_Name(w):##获取姓名
-    Names=w[0:w.index(',')]
-    return Names
 
 
 def fall(addresss):
-    #匹配省份
-    j=re.match(r'.*?(省)',addresss) #省
-    if j==None:
-        d1=""
-    else:
-        d1=j.group()
-        addresss=re.sub(d1,"",addresss,1)
+    sheng=get_province()
+    kk=0
+    for i in range(len(sheng[0])):
+        j=re.match(sheng[0][i],addresss)
+        if(j):
+            kk=i
+            d1=j.group()
+            addresss=re.sub(d1,"",addresss,1)
+            if i<4:
+                 d2=d1+'市'
+                 if re.match(r'市',addresss):
+                     addresss=re.sub(r'市',"",addresss,1)
+            if i>3:  
+                 d1=d1+"省"
+                 if re.match(r'省',addresss):
+                     addresss=re.sub(r'省',"",addresss,1)              
+            break
+                
+
+    
+    #j=re.match(r'.*?(省)',addresss) #省
+    #if j==None:
+    #    d1=""
+    #else:
+    #    d1=j.group()
+    #    addresss=re.sub(d1,"",addresss,1)
     
     #匹配市
-    j=re.match(r'.*?(市)',addresss)
-    if j==None:
-        d2=""
-    else:
-        d2=j.group()
-        addresss=re.sub(d2,"",addresss,1)
+    if kk>3:
+        j=re.match(r'.*?(市)',addresss)
+        if j==None:
+            d2=""
+        else:
+            d2=j.group()
+            addresss=re.sub(d2,"",addresss,1)
     
     #匹配县
-    j=re.match(r'.*?(县|区|市)',addresss)
+    j=re.match(r'.*?(市|区|县)',addresss)
     if j==None:
         d3=""
     else:
@@ -42,7 +54,7 @@ def fall(addresss):
         addresss=re.sub(d3,"",addresss,1)
         
     #匹配镇
-    j=re.match(r'.*?(街道|镇|乡)',addresss)
+    j=re.match(r'.*?(镇|街道|乡)',addresss)
     if j==None:
         d4=""
     else:
@@ -50,7 +62,7 @@ def fall(addresss):
         addresss=re.sub(d4,"",addresss,1)
         
     #匹配路
-    j=re.match(r'.*?(路|街|巷)',addresss)
+    j=re.match(r'.*?(街|路|巷)',addresss)
     if j==None:
         d5=""
     else:
@@ -75,6 +87,19 @@ def fall(addresss):
     eed.append(d6)
     eed.append(d7)
     return eed
+def get_address(w):##获取地址
+    addresss=re.sub(r'\d{11}',"",w)
+    addresss=re.findall("\D*.*?,(.*?)\.",addresss) 
+    addresss=("".join(addresss))
+    return(fall(addresss))
+
+def get_phone(w):##获取电话号码
+    number=re.findall("\d{11}",w)
+    return number
+
+def get_Name(w):##获取姓名
+    Names=w[0:w.index(',')]
+    return Names
 def end(w):##将姓名，电话号码，地址以此放进输出中
     endss=[]
     name=get_Name(w)
